a55f9ea0f0ece8700189360360d7e7b29214ff52
mattmachad <matheus.machado@pedbot.com.br>
2023-11-27 17:19:47 -0300

    last

--- front/pages/editTypes.php
+++ front/pages/editTypes.php
@@ -41,7 +41,7 @@ function getMediaType($type)
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Anicine</title>
+    <title>anicine</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
